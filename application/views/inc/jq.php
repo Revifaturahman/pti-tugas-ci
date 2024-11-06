44b0b84a99087da280057998009819f6f0a42b06
Revifaturahman <123242308+Revifaturahman@users.noreply.github.com>
2024-11-06 17:23:02 +0700

    first init github

---
+++ application/views/inc/jq.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <!-- jQuery -->
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <!-- jQuery UI -->
+    <script src="js/jquery-ui.min.js" type="text/javascript"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- Chart.js -->
+    <script src="js/chart.min.js" type="text/javascript"></script>
+    <!-- Sidebar Script -->
+    <script src="js/sidebar.js" type="text/javascript"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+</head>
+<body>
+
+</body>
+</html>
